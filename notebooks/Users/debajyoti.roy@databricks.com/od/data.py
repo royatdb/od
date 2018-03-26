798cd4d26df2c5f714b3cbdae5da681139c35db7
Debajyoti Roy <roy@databricks.com>
2018-03-26 02:01:03 -0400

    initial commit

---
+++ notebooks/Users/debajyoti.roy@databricks.com/od/data.py
@@ -0,0 +1,25 @@
+# Databricks notebook source
+display(
+  dbutils.fs.ls('/mnt/roy/object-detection')
+)
+
+# COMMAND ----------
+
+display(
+  dbutils.fs.ls('/mnt/roy/object-detection/images')
+)
+
+# COMMAND ----------
+
+display(
+  dbutils.fs.ls('/mnt/roy/object-detection/model_data')
+)
+
+# COMMAND ----------
+
+display(
+  dbutils.fs.ls('/mnt/roy/object-detection/out')
+)
+
+# COMMAND ----------
+
