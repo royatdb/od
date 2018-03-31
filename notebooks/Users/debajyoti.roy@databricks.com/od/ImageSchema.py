acddb44add3f41a4d7cd6057929cbe5b6e4ee15d
Debajyoti Roy <roy@databricks.com>
2018-03-31 14:24:41 -0400

    reveal with boxes

--- notebooks/Users/debajyoti.roy@databricks.com/od/ImageSchema.py
+++ notebooks/Users/debajyoti.roy@databricks.com/od/ImageSchema.py
@@ -15,6 +15,8 @@
 # MAGIC * Use `ImageSchema` to load images
 # MAGIC * Prediction `UDF` using pre-trained YOLO
 # MAGIC * Insights using `SQL`
+# MAGIC 
+# MAGIC ![yolov2](https://s3.us-east-2.amazonaws.com/databricks-roy/yolov2.jpg)
 
 # COMMAND ----------
 
@@ -74,6 +76,67 @@ def read_anchors(anchors_path):
         anchors = np.array(anchors).reshape(-1, 2)
     return anchors
   
+def generate_colors(class_names):
+    hsv_tuples = [(x / len(class_names), 1., 1.) for x in range(len(class_names))]
+    colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+    colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), colors))
+    random.seed(10101)  # Fixed seed for consistent colors across runs.
+    random.shuffle(colors)  # Shuffle colors to decorrelate adjacent classes.
+    random.seed(None)  # Reset seed to default.
+    return colors
+
+def scale_boxes(boxes, image_shape):
+    """ Scales the predicted boxes in order to be drawable on the image"""
+    height = image_shape[0]
+    width = image_shape[1]
+    image_dims = K.stack([height, width, height, width])
+    image_dims = K.reshape(image_dims, [1, 4])
+    boxes = boxes * image_dims
+    return boxes
+
+def preprocess_image(img_path, model_image_size):
+    image_type = imghdr.what(img_path)
+    image = Image.open(img_path)
+    resized_image = image.resize(tuple(reversed(model_image_size)), Image.BICUBIC)
+    image_data = np.array(resized_image, dtype='float32')
+    image_data /= 255.
+    image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
+    return image, image_data
+
+def draw_boxes(image, out_scores, out_boxes, out_classes, class_names, colors):
+    
+    font = ImageFont.truetype(font='/dbfs/mnt/roy/object-detection/font/FiraMono-Medium.otf',size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
+    thickness = (image.size[0] + image.size[1]) // 300
+
+    for i, c in reversed(list(enumerate(out_classes))):
+        predicted_class = class_names[c]
+        box = out_boxes[i]
+        score = out_scores[i]
+
+        label = '{} {:.2f}'.format(predicted_class, score)
+
+        draw = ImageDraw.Draw(image)
+        label_size = draw.textsize(label, font)
+
+        top, left, bottom, right = box
+        top = max(0, np.floor(top + 0.5).astype('int32'))
+        left = max(0, np.floor(left + 0.5).astype('int32'))
+        bottom = min(image.size[1], np.floor(bottom + 0.5).astype('int32'))
+        right = min(image.size[0], np.floor(right + 0.5).astype('int32'))
+        print(label, (left, top), (right, bottom))
+
+        if top - label_size[1] >= 0:
+            text_origin = np.array([left, top - label_size[1]])
+        else:
+            text_origin = np.array([left, top + 1])
+
+        # My kingdom for a good redistributable image drawing library.
+        for i in range(thickness):
+            draw.rectangle([left + i, top + i, right - i, bottom - i], outline=colors[c])
+        draw.rectangle([tuple(text_origin), tuple(text_origin + label_size)], fill=colors[c])
+        draw.text(text_origin, label, fill=(0, 0, 0), font=font)
+        del draw
+  
 def predict_util(array):
   model_image_size = (416, 416)
   image = Image.fromarray(array.astype('uint8'), 'RGB')
@@ -91,11 +154,26 @@ def predict_util(array):
   out_scores, out_boxes, out_classes = sess.run([scores, boxes, classes],feed_dict={yolo_model.input:image_data,K.learning_phase(): 0})
   return out_scores, out_boxes, out_classes
 
-def reveal(a_path):
-  a_row = images_df.where("image.origin='"+a_path+"'").take(1)[0][0]
-  an_array = ImageSchema.toNDArray(a_row)
-  plt.imshow(an_array)
-  display(plt.show())
+def reveal(image_file):
+  image, image_data = preprocess_image("/dbfs/mnt/roy/object-detection/images/" + image_file, model_image_size = (416, 416))
+  yolo_model = load_model("/dbfs/mnt/roy/object-detection/model_data/yolo.h5")
+  class_names = read_classes("/dbfs/mnt/roy/object-detection/model_data/coco_classes.txt")
+  anchors = read_anchors("/dbfs/mnt/roy/object-detection/model_data/yolo_anchors.txt")
+  image_shape = (720., 1280.)
+  yolo_outputs = yolo_head(yolo_model.output, anchors, len(class_names))
+  boxes, scores, classes = yolo_eval(yolo_outputs, image_shape)
+  sess = K.get_session()
+  out_scores, out_boxes, out_classes = sess.run([scores, boxes, classes],feed_dict={yolo_model.input:image_data,K.learning_phase(): 0})
+  print('Found {} boxes for {}'.format(len(out_boxes), image_file))
+  colors = generate_colors(class_names)
+  draw_boxes(image, out_scores, out_boxes, out_classes, class_names, colors)
+  image.save(os.path.join("/dbfs/mnt/roy/object-detection/out", image_file), quality=90)
+  output_image = scipy.misc.imread(os.path.join("/dbfs/mnt/roy/object-detection/out", image_file))
+  imshow(output_image)
+  fig = plt.figure()
+  img = Image.open("/dbfs/mnt/roy/object-detection/out/" + image_file)
+  plt.imshow(img)
+  display(fig)
 
 # COMMAND ----------
 
@@ -545,6 +623,10 @@ print("Boxes: "+str(out_boxes))
 
 # COMMAND ----------
 
+reveal("test.jpg")
+
+# COMMAND ----------
+
 # MAGIC %md
 # MAGIC # Prediction as a UDF
 
@@ -635,11 +717,17 @@ display(coco_df)
 
 # COMMAND ----------
 
-# MAGIC %sql SELECT classes, scores from drive where origin='dbfs:/mnt/roy/object-detection/images/0005.jpg'
+# MAGIC %sql 
+# MAGIC SELECT 
+# MAGIC   b.origin, b.class 
+# MAGIC FROM 
+# MAGIC   ( SELECT origin, explode(classes) AS class FROM  drive ) b, coco c
+# MAGIC WHERE
+# MAGIC   b.class=c.id AND c.value='car'
 
 # COMMAND ----------
 
-reveal("dbfs:/mnt/roy/object-detection/images/0005.jpg")
+reveal("0005.jpg")
 
 # COMMAND ----------
 
@@ -648,11 +736,17 @@ reveal("dbfs:/mnt/roy/object-detection/images/0005.jpg")
 
 # COMMAND ----------
 
-# MAGIC %sql SELECT classes, scores from drive where origin='dbfs:/mnt/roy/object-detection/images/0102.jpg'
+# MAGIC %sql 
+# MAGIC SELECT 
+# MAGIC   b.origin, b.class 
+# MAGIC FROM 
+# MAGIC   ( SELECT origin, explode(classes) AS class FROM  drive ) b, coco c
+# MAGIC WHERE
+# MAGIC   b.class=c.id AND c.value='bus'
 
 # COMMAND ----------
 
-reveal("dbfs:/mnt/roy/object-detection/images/0102.jpg")
+reveal("0102.jpg")
 
 # COMMAND ----------
 
@@ -661,11 +755,17 @@ reveal("dbfs:/mnt/roy/object-detection/images/0102.jpg")
 
 # COMMAND ----------
 
-# MAGIC %sql SELECT classes, scores from drive where origin='dbfs:/mnt/roy/object-detection/images/0045.jpg'
+# MAGIC %sql 
+# MAGIC SELECT 
+# MAGIC   b.origin, b.class 
+# MAGIC FROM 
+# MAGIC   ( SELECT origin, explode(classes) AS class FROM  drive ) b, coco c
+# MAGIC WHERE
+# MAGIC   b.class=c.id AND c.value='traffic light'
 
 # COMMAND ----------
 
-reveal("dbfs:/mnt/roy/object-detection/images/0045.jpg")
+reveal("0045.jpg")
 
 # COMMAND ----------
 
@@ -674,11 +774,14 @@ reveal("dbfs:/mnt/roy/object-detection/images/0045.jpg")
 
 # COMMAND ----------
 
-# MAGIC %sql SELECT classes, scores from drive where origin='dbfs:/mnt/roy/object-detection/images/0071.jpg'
+# MAGIC %sql 
+# MAGIC SELECT 
+# MAGIC   b.origin, b.class 
+# MAGIC FROM 
+# MAGIC   ( SELECT origin, explode(classes) AS class FROM  drive ) b, coco c
+# MAGIC WHERE
+# MAGIC   b.class=c.id AND c.value='truck'
 
 # COMMAND ----------
 
-reveal("dbfs:/mnt/roy/object-detection/images/0071.jpg")
-
-# COMMAND ----------
-
+reveal("0071.jpg")
